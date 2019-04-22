a060cac7f6c0f1e94e9a9ff3613b2f40e4bae6cf
tttx <kevin_wangqi@126.com>
2019-04-22 22:26:26 +0800

    Eureka集群

---
+++ microservice-eureka-7002/src/main/java/com/kevin/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,21 @@
+package com.kevin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @类名: EurekaServer7002_App<br />
+ * @包名：com.kevin.springcloud<br/>
+ * @作者：kevin<br/>
+ * @时间：2019/4/14 20:44<br/>
+ * @版本：1.0<br/>
+ * @描述：<br/>
+ */
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类，接受其他微服务注册进来
+public class EurekaServer7002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
