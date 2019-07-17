05432c46f96c44525c62c078a6e1bee286b39efb
tttx <kevin_wangqi@126.com>
2019-07-17 22:46:53 +0800

    springcloud config

---
+++ microservice-config-3344/src/main/java/com/kevin/sringcloud/Config3344_APP.java
@@ -0,0 +1,21 @@
+package com.kevin.sringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @类名: Config3344_APP<br />
+ * @包名：com.kevin.sringcloud<br/>
+ * @作者：kevin<br/>
+ * @时间：2019/7/17 21:38<br/>
+ * @版本：1.0<br/>
+ * @描述：<br/>
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config3344_APP {
+    public static void main(String[] args) {
+        SpringApplication.run(Config3344_APP.class, args);
+    }
+}
